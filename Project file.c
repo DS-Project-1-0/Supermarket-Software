2a0306ca755c31c0ccad7104bf69be7b9888958b
nimo0701 <63576190+nimo0701@users.noreply.github.com>
2020-05-01 02:11:00 +0530

    Update Project file.c

--- Project file.c
+++ Project file.c
@@ -4,19 +4,18 @@
 #include<windows.h>
 #include<string.h>
 #define size 5   //size of the password
-#include"project.h"
+#include"project.h" // includes the header file
 
 
 int main()
 {
-	char ch;
-	setColor(43);//sets the text color to blue
+	char ch;// takes the choice from the user if he is customer or admin
+	setColor(43);
 	printf("\t\t\t\t\t\t WELCOME TO SHOPIFY PLAZA	\n	");
-	setColor(15);//again sets the text color to white
-	printf("\n Are you admin(a) or customer(c) \n ");
-	printf("\n Enter your choice:	");
+	setColor(15);
+    	printf("\n Are you admin(a) or customer(c) \n ");
 	scanf("%c",&ch);
-	screen1(ch);
+	screen1(ch); // calls the login page
 	return 0;
 }
 
